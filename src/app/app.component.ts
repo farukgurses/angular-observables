215352a215f6c440a9c094ddb96ad42e7e33b7ed
Ahmet Faruk Gürses <ahmetfaruk.gurses@vbt.com.tr>
2021-10-13 19:48:29 +0300

    cross component data transfer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,6 @@
+import { Subscription } from 'rxjs';
+import { OnDestroy } from '@angular/core';
+import { UserService } from './user.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false
+  private activatedSubscribtion: Subscription
 
-  ngOnInit() {}
+  constructor(private userService: UserService) {}
+ 
+  ngOnInit() {
+   this.activatedSubscribtion = this.userService.activatedEmitter.subscribe(didActivate =>{
+      this.userActivated = didActivate
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.activatedSubscribtion.unsubscribe()
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Observable, Subscription } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObservaleSubscription: Subscription;
+  constructor() {}
 
   ngOnInit() {
+    // this.firstObservaleSubscription = interval(1000).subscribe(count=>{
+    //     console.log(count)
+    // })
+    const customIntervalObservable = Observable.create((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error("Count is greater than3!"));
+        }
+        count++;
+      }, 1000);
+    });
+
+    // customIntervalObservable.pipe(map((data: number)=>{
+    //     return 'Round ' + (data + 1)
+    // }))
+
+    this.firstObservaleSubscription = customIntervalObservable
+      .pipe(
+        map((data: number) => {
+          return "Round: " + (data + 1);
+        })
+      )
+      .subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (error) => {
+          console.log(error());
+        },
+        () => {
+          console.log("completed");
+        }
+      );
   }
 
+  ngOnDestroy() {
+    this.firstObservaleSubscription.unsubscribe();
+  }
 }
